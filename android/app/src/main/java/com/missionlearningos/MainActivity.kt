587f6516044e5b27c128a6ed87c339fda2b82155
mohrezashahbazi-a11y <mohrezashahbazi@gmail.com>
2026-08-17 17:02:42 +0330

    feat: add standalone Android local WebView activity

---
+++ android/app/src/main/java/com/missionlearningos/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.missionlearningos
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+        setContentView(webView)
+    }
+}
